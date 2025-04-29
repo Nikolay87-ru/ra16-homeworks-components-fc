139a78ef8529919082624fd423b71fda97e40e3f
Neto test <nikolajeliseev834@gmail.com>
2025-04-29 18:23:06 +0300

    добавлен html-структура элемента сайта из условий задачи

--- my-app/src/components/ShopItemFunc.tsx
+++ my-app/src/components/ShopItemFunc.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import { ShopItem } from './types'
+import React from "react";
+import { ShopItem } from "./types";
 
 interface ShopItemProps {
   item: ShopItem;
 }
 
 const ShopItemFunc: React.FC<ShopItemProps> = ({ item }) => {
-  
+  return (
+    <div className="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div className="description">{item.descriptionFull}</div>
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay"></div>
+      </div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{`${item.currency}${item.price.toFixed(2)}`}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  );
 };
 
 export default ShopItemFunc;
