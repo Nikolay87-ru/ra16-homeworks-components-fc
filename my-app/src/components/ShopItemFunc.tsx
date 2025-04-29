ca254237d7d66d90bdbcd6037f16d91de3644bcc
Neto test <nikolajeliseev834@gmail.com>
2025-04-29 18:19:42 +0300

    добавлены импорты

---
+++ my-app/src/components/ShopItemFunc.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ShopItem } from './types'
+
+interface ShopItemProps {
+  item: ShopItem;
+}
+
+const ShopItemFunc: React.FC<ShopItemProps> = ({ item }) => {
+  
+};
+
+export default ShopItemFunc;
